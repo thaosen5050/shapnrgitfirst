d7eda78aba4004d963614d0fa13ed2944ea2f993
thaosen5050 <rohit.thaosen5050@gmail.com>
2023-08-08 20:51:25 +0530

    element by tag

--- dom.js
+++ dom.js
@@ -1,8 +1,14 @@
 
-let test=document.getElementsByClassName('list-group-item')
+let test=document.getElementsByTagName('li')
 
-test[2].style.backgroundColor='green'
+
 for(let i=0;i<test.length;i++){
-    test[i].style.fontWeight='bold'
+    
     test[i].style.color='red'
+}
+
+let test2=document.getElementsByClassName('list-group-item')
+for(let i=0;i<test2.length;i++){
+    
+    test2[i].style.color='blue'
 }
