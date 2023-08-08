c4bc0c1864be2633fb0bd8b215aa8266568dbe36
thaosen5050 <141542695+thaosen5050@users.noreply.github.com>
2023-08-08 18:52:45 +0530

    Delete dom.js

--- dom.js
+++
@@ -1,9 +0,0 @@
-
-
-let htitle=document.getElementById('header-title')
-htitle.style.borderBottom='solid 5px #000'
-
-let add=document.querySelectorAll('.title')
-
-add[0].style.fontWeight='bold'
-add[0].style.color='green'
