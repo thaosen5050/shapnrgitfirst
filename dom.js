20195ce807c8dd759e85e82df638724bbb5da3a9
thaosen5050 <rohit.thaosen5050@gmail.com>
2023-08-09 13:21:53 +0530

    node creation

--- dom.js
+++ dom.js
@@ -1,14 +1,50 @@
+let itemList=document.querySelector('#items');
+//console.log(itemList);
 
-let test=document.querySelector('.list-group-item:nth-child(2)')
-test.style.backgroundColor='green'
-let test2=document.querySelector('.list-group-item:nth-child(3)')
-test2.style.display='none'
-let test3=document.querySelectorAll('li')
-test3[1].style.color='green'
-let test4=document.querySelectorAll('li:nth-child(odd)')
+//itemList.parentNode.style.backgroundColor='#f4f4f4';
+//itemList.parentElement.style.backgroundColor='red';
+//console.log(itemList.childNodes);
 
-for(let i=0;i<test4.length;i++){
-    
-    test4[i].style.backgroundColor='green'
-}
+//console.log(itemList.children);
+//itemList.children[2].style.backgroundColor='blue';
+
+//console.log(itemList.firstChild);
+
+//console.log(itemList.firstElementChild);
+//itemList.firstElementChild.textContent='hello';
+
+//console.log(itemList.lastChild);
+
+//console.log(itemList.lastElementChild);
+//itemList.lastElementChild.textContent='hello 2';
+
+//console.log(itemList.nextSibling);
+//console.log(itemList.nextElementSibling);
+
+//console.log(itemList.previousSibling);
+//console.log(itemList.previousElementSibling);
+//itemList.previousElementSibling.style.color='green';
+
+let newDiv=document.createElement('div');
+newDiv.className='hello';
+newDiv.id='hello1';
+newDiv.setAttribute('title' ,'Hello div');
+let newDivText= document.createTextNode('HEllo');
+newDiv.appendChild(newDivText);
+console.log(newDiv);
+let contain=document.querySelector('header .container');
+let hh1=document.querySelector('header h1');
+
+contain.insertBefore(newDiv, hh1);
+
+
+
+let newDiv2=document.createElement('div');
+newDiv2.className='list-group-item';
+let newText2= document.createTextNode('HEllo');
+let newDiv2Text= document.createTextNode('HEllo');
+newDiv2.appendChild(newDiv2Text);
+itemList.insertBefore(newDiv2, itemList.children[0]);
+
+
 
