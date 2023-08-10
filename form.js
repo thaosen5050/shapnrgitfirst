1dc13e02406f51488f6e2243ac05693c71284095
thaosen5050 <rohit.thaosen5050@gmail.com>
2023-08-10 10:05:55 +0530

    scale users

--- form.js
+++ form.js
@@ -1,6 +1,7 @@
 let form=document.getElementById('my-form');
 //console.log(localStorage.getItem('name1'))
 //console.log(JSON.parse(localStorage.getItem('name1')))
+let userlist=document.getElementById('users')
 let x=1;
 form.addEventListener('submit' , formSub);
 
@@ -13,8 +14,13 @@ function formSub(e){
     }
 
     let objStrng=JSON.stringify(obj);
-    localStorage.setItem(`name${x}` , objStrng)
+    localStorage.setItem( Mail , objStrng)
     x=x+1;
     e.preventDefault();
     //console.log(Name)
+    let newName=document.createElement('li');
+    newName.className='item';
+    newName.appendChild(document.createTextNode('Name: '+Name+' '+' Email: '+Mail));
+    userlist.appendChild(newName);
+
 }
