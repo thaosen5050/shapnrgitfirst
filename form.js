3f2c3738e28c235334a86bf1f7b7d5bb15427e9d
thaosen5050 <rohit.thaosen5050@gmail.com>
2023-08-10 09:45:57 +0530

    object store

--- form.js
+++ form.js
@@ -1,13 +1,20 @@
 let form=document.getElementById('my-form');
-console.log(localStorage.getItem('name3'))
+//console.log(localStorage.getItem('name1'))
+//console.log(JSON.parse(localStorage.getItem('name1')))
 let x=1;
 form.addEventListener('submit' , formSub);
 
 function formSub(e){
     let Name=document.getElementById('name').value;
     let Mail=document.getElementById('email').value;
-    localStorage.setItem(`name${x}` , Name+' '+Mail)
+    let obj={
+        nameobj : Name ,
+        mailobj : Mail
+    }
+
+    let objStrng=JSON.stringify(obj);
+    localStorage.setItem(`name${x}` , objStrng)
     x=x+1;
     e.preventDefault();
-    console.log(Name)
+    //console.log(Name)
 }
