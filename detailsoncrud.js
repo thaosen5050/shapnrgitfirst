728e963b4fdd2bba3ec449aaf0780b25c6e30861
thaosen5050 <rohit.thaosen5050@gmail.com>
2023-09-03 13:14:49 +0530

    CrudEdit

--- detailsoncrud.js
+++ detailsoncrud.js
@@ -7,6 +7,7 @@ form.addEventListener('submit' , formSub);
 userList.addEventListener('click' , delUser);
 userList.addEventListener('click' , editUser);
 
+
 axios.get('https://crudcrud.com/api/4017a8c2370f4e20acfaa604ad9fb532/aplicationData')
 .then(res=>{
     let axobj=res.data;
@@ -37,7 +38,7 @@ axios.get('https://crudcrud.com/api/4017a8c2370f4e20acfaa604ad9fb532/aplicationD
 
 
 
-function formSub(e){
+function formSub(e ){
     let Name=document.getElementById('name').value;
     let Mail=document.getElementById('email').value;
 
@@ -46,9 +47,13 @@ function formSub(e){
        Name ,
         Mail 
     };
-    axios.post('https://crudcrud.com/api/4017a8c2370f4e20acfaa604ad9fb532/aplicationData' , obj)
-    .then(res=>console.log(res))
-    .catch(err=>console.log(err))
+    
+        axios.post('https://crudcrud.com/api/4017a8c2370f4e20acfaa604ad9fb532/aplicationData' , obj)
+        .then(res=>console.log(res))
+        .catch(err=>console.log(err))
+    
+    
+    
     
     e.preventDefault();
 
@@ -108,9 +113,30 @@ function editUser(e){
     document.getElementById('name').value=oldName;
     document.getElementById('email').value=oldMail;
 
-
+    axios.get('https://crudcrud.com/api/4017a8c2370f4e20acfaa604ad9fb532/aplicationData')
+    .then(res=>{
+    let axobj=res.data;
+    
+    axobj.forEach((x)=>{
+            let crudName=x.Name;
+            let crudMail=x.Mail;
+            let id=x._id;
+            if (oldMail==crudMail){
+                let obj={
+                    crudName,
+                    crudMail,
+                };
+                
+                 axios.delete(`https://crudcrud.com/api/4017a8c2370f4e20acfaa604ad9fb532/aplicationData/${id}`)
+                 .then(res=console.log(obj))
+                
+            }
+        })
+    })
+    .catch(err=>console.log(err))
 
    // localStorage.removeItem(edtEle.firstChild.nextSibling.textContent)
-
+    
     userList.removeChild(edtEle);
 }
+
