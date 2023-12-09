4296a15f64e5c3f8b895adcbd5335de3f096527b
thaosen5050 <rohit.thaosen5050@gmail.com>
2023-12-09 13:31:06 +0530

    req res

--- bck_app.js
+++ bck_app.js
@@ -1,8 +1,35 @@
 const http=require('http')
 
 const server = http.createServer((req,res)=>{
-    console.log('Rohit')
-   //console.log(req)
+    
+    const url=req.url
+    if (url==='/home'){
+        res.write('<html>')
+        res.write('<head><title>Home page</title></head>')
+        res.write('<body><h1>Welcome home</h1></body>')
+        res.write('</html>')
+        return res.end()
+    }
+    if (url==='/about'){
+        res.write('<html>')
+        res.write('<head><title>About page</title></head>')
+        res.write('<body><h1>Welcome to about us page</h1></body>')
+        res.write('</html>')
+        return res.end()
+    }
+    if (url==='/node'){
+        res.write('<html>')
+        res.write('<head><title>Node page</title></head>')
+        res.write('<body><h1>Welcome to my Node.js project</h1></body>')
+        res.write('</html>')
+        return res.end()
+    }
+    res.setHeader('Content-Type' , 'text/html')
+    res.write('<html>')
+    res.write('<head><title>My default page</title></head>')
+    res.write('<body><h1>Welcome to my dd Node.js project</h1></body>')
+    res.write('</html>')
+    res.end()
 })
 
 server.listen(4000)
