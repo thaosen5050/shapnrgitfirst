a467f64501ee03341f5b42decdb5df7284acf612
thaosen5050 <rohit.thaosen5050@gmail.com>
2023-12-16 19:06:43 +0530

    redirect

--- bck_app.js
+++ bck_app.js
@@ -1,8 +1,9 @@
 const http=require('http')
+const fs=require('fs')
 
 const server = http.createServer((req,res)=>{
-    
     const url=req.url
+    const method=req.method
     if (url==='/home'){
         res.write('<html>')
         res.write('<head><title>Home page</title></head>')
@@ -22,6 +23,19 @@ const server = http.createServer((req,res)=>{
         res.write('<head><title>Node page</title></head>')
         res.write('<body><h1>Welcome to my Node.js project</h1></body>')
         res.write('</html>')
+        return res.end()
+    }
+    if (url==='/'){
+        res.write('<html>')
+        res.write('<head><title>Message</title></head>')
+        res.write('<body><form action="/message" method="POST"><input type="text" name="mess"><button type="submit">send</button></form></body>')
+        res.write('</html>')
+        return res.end()
+    }
+    if (url==='/message' && method==='POST'){
+        fs.writeFileSync('message.txt', 'Hello')
+        res.statusCode=302
+        res.setHeader('Location', '/')
         return res.end()
     }
     res.setHeader('Content-Type' , 'text/html')
