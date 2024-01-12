bef051c50ca265ea3e0883a95d4e20c8c249b9b8
thaosen5050 <rohit.thaosen5050@gmail.com>
2024-01-12 15:31:10 +0530

    chat app

--- bck_app.js
+++ bck_app.js
@@ -1,20 +1,33 @@
 const express=require('express')
 const bodyParser=require('body-parser')
+const fs=require('fs');
 
-const adminRoutes=require('./routes/admin')
-const shopRoutes=require('./routes/shop')
+const chat=express()
+ 
+chat.use(bodyParser.urlencoded({extended : false}))
 
-const app=express()
-
-app.use(bodyParser.urlencoded({extended : false}))
-
-app.use('/admin',adminRoutes)
-app.use(shopRoutes)
-
-app.use((req,res,next)=>{
-    res.status(404).send('<h1>Page not found</h1>')
+chat.get('/login' , (req,res,next)=>{
+    res.send('<form action="/details" method="POST"><input type="text" name="username"><button type="submit">login</button></form>')
 })
 
+chat.post('/details' , (req,res,next)=>{
+    fs.writeFileSync('user.txt' , req.body.username)
+    res.redirect('/')
+})
 
+chat.get('/' , (req,res,next)=>{
+    fs.readFile('chat.txt' , 'utf-8' , (err,data)=>{
+    res.send(`<form action="/chat" method="POST"><label for="history">${data}</label><br><input type="text" id="history" name="text"><button type="submit">send</button></form>`)
+   })
+}) 
 
-app.listen(3000)
+chat.post('/chat' , (req,res,next)=>{
+    fs.readFile('chat.txt' , 'utf-8' , (err,data)=>{
+        let user=fs.readFileSync('user.txt' ,'utf-8')
+        let content=data.replace(data,data+'  '+user+':'+req.body.text)
+        fs.writeFileSync('chat.txt' , content, 'utf-8')
+        res.redirect('/')
+    })
+})
+
+chat.listen(4000)
