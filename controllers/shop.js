7c2610157159ced140f8d21432c4648a1d39825f
thaosen5050 <rohit.thaosen5050@gmail.com>
2024-02-23 22:29:24 +0530

    dynamic 2

--- controllers/shop.js
+++ controllers/shop.js
@@ -1,4 +1,5 @@
 const Product = require('../models/product');
+const Cart=require('../models/cart');
 
 exports.getProducts = (req, res, next) => {
   Product.fetchAll(products => {
@@ -36,6 +37,15 @@ exports.getCart = (req, res, next) => {
     path: '/cart',
     pageTitle: 'Your Cart'
   });
+};
+
+exports.postCart =(req,res,next)=>{
+    const prodID=req.body.productId;
+    Product.findById(prodID , (product)=>{
+        Cart.addProduct(prodID , product.price);
+    })
+    
+    res.redirect('/cart')
 };
 
 exports.getOrders = (req, res, next) => {
